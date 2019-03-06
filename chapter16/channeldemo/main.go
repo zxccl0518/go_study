46d84c45e60a76afb6458979f2b0316a1b11801c
jackie.zhao <jackie.zhao@robo-sys.com>
2019-03-06 08:19:23 +0800

    2019_03_06 go study 08:19

---
+++ chapter16/channeldemo/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 演示一下管道的使用
+	// 1.创建一个可以存放3个int类型的管道
+	var intChan chan int
+	intChan = make(chan int, 3)
+
+	// 看看 管道是什么
+	fmt.Printf("intChan 的值=%v, intChan本身的地址=%p\n", intChan, &intChan)
+
+	// 向管道写入数据
+	intChan <- 10
+	num := 211
+	intChan <- num
+
+	// 看看管道的长度和cap容量. 管道容量不会自增长，多了就爆了
+	fmt.Printf("channel len = %v  cap = %v\n", len(intChan), cap(intChan))
+
+	var num2 int
+	num2 = <-intChan
+	fmt.Println("num2 = ", num2)
+	fmt.Printf("channel len = %v  cap = %v\n", len(intChan), cap(intChan))
+
+	//在没有使用 协程的情况下，如过我们的管道数据已经全部取出，再去就会报告deadlock
+
+	// num3 := <-intChan
+	// num4 := <-intChan
+	// fmt.Println("num 3 = ", num3, "  < --- > num4 = ", num4) //没有内容继续取 报错： fatal error: all goroutines are asleep - deadlock!
+
+}
