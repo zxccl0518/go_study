a5cbf8ababe4363dd8fb2fbdccdc8a23aab24cd8
jackie.zhao <jackie.zhao@robo-sys.com>
2019-04-20 14:28:34 +0800

    对栈的理解。

---
+++ chapter20/stack/main.go
@@ -0,0 +1,16 @@
+package main
+
+// 使用数组来模拟一个栈的使用。
+type Stack struct {
+	MaxTop int    // 表示我们栈最大都可以存储的个数。
+	Top    int    // 表示栈顶，因为我们栈顶固定，因此我们使用top
+	arr    [5]int // 数组模拟栈
+}
+
+// 入栈操作。
+func (this *Stack) Push() {
+
+}
+func main() {
+
+}
